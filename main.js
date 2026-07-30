30ff107d63beab5a5d2e8168f78e4b30a8c15fe2
emilinda1 <emily.carolaine.antunes@escola.pr.gov.br>
2026-07-30 13:29:28 -0300

    111

--- main.js
+++ main.js
@@ -57,5 +57,17 @@ let senha = '';
         senha = senha + letrasMaiusculas[numeroAleatorio];
     }
     campoSenha.value = senha;
-    
+    classificaSenha();
+
+}
+
+function classificaSenha(){
+    forcaSenha.classList.remove('fraca','media','forte');
+    if (tamanhoSenha > 11){
+        forcaSenha.classList.add('forte');
+    } else if (tamanhoSenha > 5 && tamanhoSenha < 12 ) {
+        forcaSenha.classList.add('media');
+    } else if (tamanhoSenha <= 5){
+        forcaSenha.classList.add('fraca');
+    }
 }
